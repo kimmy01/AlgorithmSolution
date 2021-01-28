965d7c244384bfa5205dd62797cdd32ae00f0f8b
kimmy01 <nkim40181@hanmail.net>
2021-01-28 21:35:02 +0900

    백준 1330 두 수 비교하기

---
+++ 0128/TwoNumbers.java
@@ -0,0 +1,29 @@
+package com.algo0128;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class TwoNumbers {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		String str = br.readLine();
+		
+		StringTokenizer stk = new StringTokenizer(str);
+		int A = Integer.parseInt(stk.nextToken());
+		int B = Integer.parseInt(stk.nextToken());
+		
+		if(A>B) {
+			System.out.println(">");
+		}else if(A<B) {
+			System.out.println("<");
+		}else {
+			System.out.println("==");
+		}
+	}
+}
