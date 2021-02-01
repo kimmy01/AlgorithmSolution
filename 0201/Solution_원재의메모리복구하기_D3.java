7f3f3b54feaa1abecee3dfdc47f72536626d5fa3
kimmy01 <nkim40181@hanmail.net>
2021-02-01 13:33:15 +0900

    SWEA 1289 원재의 메모리 복구하기 D3

---
+++ 0201/Solution_원재의메모리복구하기_D3.java
@@ -0,0 +1,37 @@
+package com.ssafy.ws;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Solution_원재의메모리복구하기_D3 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		String str = br.readLine();
+		int T = Integer.parseInt(str);
+		char[] array;
+		
+		for(int i=0; i<T; i++) {
+			int count = 0;
+			str = br.readLine();
+			array = new char[str.length()];
+			array = str.toCharArray();
+			
+			if(array[0] == '1') {
+				count++;
+			}
+			for(int j=1; j<array.length; j++) {
+				if(array[j] != array[j-1]) {
+					count++;
+				}else {
+					continue;
+				}
+			}
+			System.out.printf("#%d %d\n",i+1 ,count);
+		}
+	}
+}
