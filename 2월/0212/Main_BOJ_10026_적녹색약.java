72d6064ca3f3661195ce2407625a38c86f122db4
kimmy01 <nkim40181@hanmail.net>
2021-02-12 23:41:32 +0900

    백준 10026 적녹색약

---
+++ 2월/0212/Main_BOJ_10026_적녹색약.java
@@ -0,0 +1,10 @@
+package com.victoria;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main_BOJ_10026_적녹색약 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    }
+}
