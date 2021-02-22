2caf768a9ba57c3bd03f13560d9de041982ed4f2
kimmy01 <nkim40181@hanmail.net>
2021-02-22 23:09:49 +0900

    백준 17299 오등큰수

--- 2월/0222/Main_BOJ_17299_오등큰수.java
+++ 2월/0222/Main_BOJ_17299_오등큰수.java
@@ -1,6 +1,5 @@
 package com.algo0222;
 import java.io.*;
-import java.util.ArrayList;
 import java.util.Stack;
 import java.util.StringTokenizer;
 public class Main_BOJ_17299_오등큰수 {
@@ -10,45 +9,28 @@ public class Main_BOJ_17299_오등큰수 {
         StringBuilder sb = new StringBuilder();
         int N = Integer.parseInt(br.readLine());
         StringTokenizer stk = new StringTokenizer(br.readLine(), " ");
-        Stack<Integer> stack = new Stack<>();
-        int[][] numbers = new int[N][3]; //0:인덱스 1:숫자 2:등장횟수
+        Stack<Integer> stacks = new Stack<>();
+        int[] list = new int[N];
+        int[] num = new int[1000001];
+        int[] NGF2 = new int[N];
         int count = 0;
         for(int i=0; i<N; i++){
-            stack.push(i);
-            numbers[i][0] = i;
-            numbers[i][1] = Integer.parseInt(stk.nextToken());
-            for(int j=0; j<N; j++){
-                if(numbers[j][1] == numbers[i][1]){
-                    count = ++numbers[j][2];
-                    break;
-                }
-            }
-            for(int j=0; j<N; j++){
-                if(numbers[j][1] == numbers[i][1]){
-                    numbers[j][2] = count;
-                }
-            }
+            list[i] = Integer.parseInt(stk.nextToken());
+            num[list[i]]++;
         }
-        int[] NGF = new int[N];
-        ArrayList<Integer> saved = new ArrayList<>();//스택에서 꺼내온 애 담기
-        int index = 0; //while문 인덱스
-        while(!stack.isEmpty()){
-            int temp = stack.pop();
-            if(saved!=null){
-                for(int i=0; i<saved.size(); i++){
-                    if(numbers[saved.get(i)][2] > numbers[temp][2]){
-                        NGF[temp] = numbers[saved.get(i)][1];
-                    }
-                }
-                if(NGF[temp]==0){
-                    NGF[temp]=-1;
-                }
-            }
-            saved.add(temp);
-            index++;
+        for (int k = N-1; k >= 0; k--) {
+            while (!stacks.isEmpty() && num[list[k]] >= num[stacks.peek()])
+                stacks.pop();
+
+            if (stacks.isEmpty())
+                NGF2[k] = -1;
+            else
+                NGF2[k] = stacks.peek();
+
+            stacks.push(list[k]);
         }
-        for(int i=0; i<N; i++){
-            sb.append(NGF[i]+" ");
+        for(int j=0; j<N; j++){
+            sb.append(NGF2[j]+" ");
         }
         bw.write(sb.toString());
         bw.flush();
