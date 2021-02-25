1f1eeae4bbe53f8acfee70ac4c39ffdbec304f47
kimmy01 <nkim40181@hanmail.net>
2021-02-25 22:32:52 +0900

    백준 3985 롤케이크

--- 2월/0221/Main_3985_롤케이크.java
+++ 2월/0221/Main_3985_롤케이크.java
@@ -16,36 +16,33 @@ public class Main_3985_롤케이크 {
         for(int i=1; i<=N; i++){
             String temp = br.readLine();
             StringTokenizer stk = new StringTokenizer(temp, " ");
-            int start = stoi(stk.nextToken());
-            int finish = stoi(stk.nextToken());
-            cakeL[i] = finish-start+1;
-            maxL = Math.max(cakeL[i], maxL);
+            int start = Integer.parseInt(stk.nextToken()); //케이크 시작
+            int finish = Integer.parseInt(stk.nextToken()); //케이크 끝
+            cakeL[i] = finish-start+1; //케이크 길이
+            maxL = Math.max(cakeL[i], maxL); //제일 긴 케이크 원하는 크기
             for(int j=start; j<=finish; j++){
-                if(!cake[j]){
-                    cake[j]=true;
-                    people[i]++;
+                if(!cake[j]){ //케이크가 남아있다면
+                    cake[j]=true; 
+                    people[i]++; //가져가기
                 }
             }
         }
         for(int i=1; i<=N; i++){
-            if(realMax < people[i]) {
+            if(realMax < people[i]) { //제일 많은 케이크 크기 찾기
                 realMax = people[i];
             }
         }
-        for(int i=1; i<=N; i++){
+        for(int i=1; i<=N; i++){ //가장 많은 케이크 기대하는 방청객 번호
             if(maxL == cakeL[i]){
                 System.out.println(i);
                 break;
             }
         }
-        for(int i=1; i<=N; i++){
+        for(int i=1; i<=N; i++){ //실제로 가장 많은 케이크 받은 방청객 번호
             if(realMax == people[i]){
                 System.out.println(i);
                 break;
             }
         }
-    }
-    static int stoi(String s){
-        return Integer.parseInt(s);
     }
 }
