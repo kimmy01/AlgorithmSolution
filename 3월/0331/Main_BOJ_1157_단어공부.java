3aeeb33cdd49e6470aa0f252db36148bc8ffbc34
kimmy01 <nkim40181@hanmail.net>
2021-03-31 22:52:51 +0900

    백준 1157 단어 공부

---
+++ 3월/0331/Main_BOJ_1157_단어공부.java
@@ -0,0 +1,46 @@
+package com.algo0331;
+
+import java.util.*;
+import java.io.*;
+
+public class Main_BOJ_1157_단어공부 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        s = s.toUpperCase();
+        HashMap<Character, Integer> alphabets = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (alphabets.containsKey(s.charAt(i))) {
+                int temp = alphabets.get(s.charAt(i)) + 1;
+                alphabets.put(s.charAt(i), temp);
+                continue;
+            }
+            alphabets.put(s.charAt(i), 1);
+        }
+        int max = 0;
+        char maximum = 'a';
+        Iterator<Character> iterator = alphabets.keySet().iterator();
+        while (iterator.hasNext()) {
+            char key = iterator.next();
+            int temp = alphabets.get(key);
+            if (temp > max) {
+                maximum = key;
+                max = temp;
+            }
+        }
+        int flag = 0;
+        iterator = alphabets.keySet().iterator();
+        while (iterator.hasNext()) {
+            char key = iterator.next();
+            int temp = alphabets.get(key);
+            if (temp == max && key != maximum) {
+                flag++;
+            }
+        }
+        if (flag == 0) {
+            System.out.println(maximum);
+        } else {
+            System.out.println('?');
+        }
+    }
+}
