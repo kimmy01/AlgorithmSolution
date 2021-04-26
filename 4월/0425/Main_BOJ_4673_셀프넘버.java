37f1d591d045c94d7f6b73992f4cbdb1ffaaa4fe
kimmy01 <nkim40181@hanmail.net>
2021-04-26 22:54:05 +0900

    백준 4673 셀프넘버

--- 4월/0425/Main_BOJ_4673_셀프넘버.java
+++ 4월/0425/Main_BOJ_4673_셀프넘버.java
@@ -1,7 +1,29 @@
 package com.algo0426;
-import java.util.*;
 public class Main_BOJ_4673_셀프넘버 {
     public static void main(String[] args) throws Exception {
-        
+        boolean[] checked = new boolean[10001];
+
+        for(int i=1; i<=10000; i++){
+            int num = i;
+            int answer = i;
+            if(i >= 10){
+                while(true){
+                    if(num < 10) break;
+                    answer += num%10;
+                    num/=10;
+                }
+                answer += num;
+            }else{
+                answer += i;
+            }
+            if(answer <= 10000){
+                checked[answer] = true;
+            }
+        }
+
+        for(int i=1; i<=10000; i++){
+            if(!checked[i])
+                System.out.println(i);
+        }
     }
 }
