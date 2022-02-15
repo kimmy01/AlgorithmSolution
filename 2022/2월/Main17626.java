a0cb82f7a2601e4899210cd3f9b3a3dac6fd1e5a
김나영 <nkim40181@hanmail.net>
2022-02-15 14:58:38 +0900

    백준 17626 Four Squares

---
+++ 2022/2월/Main17626.java
@@ -0,0 +1,22 @@
+package com.algo2022_02;
+
+import java.io.*;
+
+public class Main17626 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] dp = new int[N+1];
+        dp[1] = 1;
+        int min = 0;
+        for(int i=2; i<=N; i++){
+            min = Integer.MAX_VALUE;
+            for(int j=1; j*j<=i; j++){
+                int tmp = i - j*j;
+                min = Math.min(min, dp[tmp]);
+            }
+            dp[i] = min + 1;
+        }
+        System.out.println(dp[N]);
+    }
+}
