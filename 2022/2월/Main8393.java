625253229fbb7cec73bde5231548523d8b83cdfe
김나영 <nkim40181@hanmail.net>
2022-02-20 15:50:25 +0900

    백준 8393 합

---
+++ 2022/2월/Main8393.java
@@ -0,0 +1,15 @@
+package com.algo2022_02;
+
+import java.io.*;
+
+public class Main8393{
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int sum = 0;
+        for(int i=1; i<=N; i++){
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+}
