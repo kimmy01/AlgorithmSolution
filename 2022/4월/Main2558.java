7b28d61f9233450a3e9a98e201bf800dad99892d
김나영 <nkim40181@hanmail.net>
2022-04-02 22:31:38 +0900

    백준 2558 A+B - 2

---
+++ 2022/4월/Main2558.java
@@ -0,0 +1,12 @@
+package com.algo2022_04;
+
+import java.io.*;
+
+public class Main2558 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+        System.out.println(A+B);
+    }
+}
